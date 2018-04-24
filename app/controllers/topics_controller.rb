13cdcbe3bf0f32a5ce15d2216814a2f5b7cefe80
dccmmtop <dccmmtop@gmail.com>
2018-04-24 17:45:22 +0800

    search topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,4 +7,13 @@ class TopicsController < ApplicationController
     @topic = Topic.friendly.find(params[:id])
     @topic.update(reading_count: @topic.reading_count + 1)
   end
+
+  def search
+    key_word = params[:search]
+    if key_word.size == 0
+      @topics = []
+    else
+      @topics = Topic.where("title like '%#{key_word}%' or body like '%#{key_word}%'")
+    end
+  end
 end
