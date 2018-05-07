03f762604a5ebd88b8a0cb1b6492e05570f71801
dccmmtop <dccmmtop@gmail.com>
2018-05-07 16:32:10 +0800

    nono

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "https://github.com/dccmmtop/script_blog.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/script_blog/"
+set :deploy_to, "~/script_blog/"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
