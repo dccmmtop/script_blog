763a8fdedb21a2a64d0b46f6066b613d45c83c9f
dccmmtop <dccmmtop@gmail.com>
2018-08-07 23:52:58 +0800

    api

--- app/api/topics_api.rb
+++ app/api/topics_api.rb
@@ -9,17 +9,21 @@ module Api
       requires :body, type: String, desc: "body"
     end
     post 'create' do
-      puts params
       title = params[:title].to_s
       tag = params[:tag].to_s
       body = params[:body].to_s
       if title.length > 0 && body.length > 0
         if params[:created_at].length > 0
-          Topic.create(title:title,tags:tag,body:body,created_at: DateTime.parse(params[:created_at]))
+          _params = {title:title,tags:tag,body:body,created_at: DateTime.parse(params[:created_at])}
         else
-          Topic.create(title:title,tags:tag,body:body) 
+          _params = {title:title,tags:tag,body:body} 
         end
-        {status:0, message: "success",data:{}}
+        if topic = Topic.find_by_title(title)
+          topic.update(_params)
+        else
+          Topic.create(_params)
+        end
+        return {status:0, message: "#{title} success",data:{}}
       else
         {status:1, message: "title or body should not null"}
       end
@@ -35,7 +39,7 @@ module Api
         return {status:2, message: "not found #{params[:title]}"}
       end
       @topic.delete
-      return {status:0}
+      return {status:0, message: "success",data:{}}
     end
 
     get :ping do
